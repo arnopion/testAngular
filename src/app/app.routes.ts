c489296867bd1b6bbfeec7f352cbca728b112fa4
Arno Pion <arno.pion@capgemini.com>
2024-11-19 14:40:58 +0100

    two components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ListComponent } from './components/list/list.component';
+import { StaticComponent } from './components/static/static.component';
 
 export const routes: Routes = [
     { path: '', component: ListComponent },
+    { path: 'static', component: StaticComponent },
 ];
--- src/app/layout-template/layout/layout.component.ts
+++ src/app/layout-template/layout/layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { SidebarModule } from 'primeng/sidebar';
 import { ButtonModule } from 'primeng/button';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [SidebarModule, ButtonModule],
+  imports: [SidebarModule, ButtonModule, RouterOutlet],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
