9fed4397e10f491afb6d1daa014e9f310cac79e1
Arno Pion <arno.pion@capgemini.com>
2024-11-19 15:13:25 +0100

    create table

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { map, Observable } from 'rxjs';
+import { User } from '../../shared/user.model';
+import { UsersActions, UserApiActions } from '../../state/users.actions';
+import { selectUsers } from '../../state/users.selectors';
+import { UsersService } from '../../state/users.service';
+import { TableModule } from 'primeng/table';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TableModule],
   templateUrl: './list.component.html',
-  styleUrl: './list.component.scss'
+  styleUrls: ['./list.component.scss'], // Fixed typo here
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
+  users$!: Observable<User[]>;
 
+  constructor(private usersService: UsersService, private store: Store) { }
+
+  onAdd(userId: string) {
+    this.store.dispatch(UsersActions.addUser({ userId }));
+  }
+
+  ngOnInit() {
+    this.users$ = this.store.select(selectUsers).pipe(
+      map(users => users || [])
+    )
+
+    this.usersService
+      .getUsers()
+      .subscribe((users) =>
+        this.store.dispatch(UserApiActions.retrievedUserList({ users }))
+      );
+  }
 }
--- src/app/state/users.selectors.ts
+++ src/app/state/users.selectors.ts
@@ -1,9 +1,9 @@
 import { createSelector, createFeatureSelector } from '@ngrx/store';
 import { User } from '../shared/user.model';
 
-export const selectUsers = createFeatureSelector<ReadonlyArray<User>>('users');
+export const selectUsers = createFeatureSelector<Array<User>>('users');
 
 export const selectUserById = createSelector(
     selectUsers,
-    (users: ReadonlyArray<User>, id: string) => users.find(user => user.id === id)
+    (users: Array<User>, id: string) => users.find(user => user.id === id)
 );
